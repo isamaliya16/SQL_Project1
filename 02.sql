ca76cf561700865b3c0278c8e06d189b8dddf3d0
Ayush Isamaliya <aisamaliya8@gmail.com>
2025-09-18 15:28:35 +0530

    02.sql file update .

--- 02.sql
+++ 02.sql
@@ -10,23 +10,15 @@ INSERT into orders (OrderID,CustomerID,OrderDate,TotalAmount)VALUES
 (2, 2, '2025-09-02', 89.99),
 (3, 3, '2025-09-05', 200.00),
 (4, 4, '2025-09-10', 49.75),
-(5, 5, '2025-09-12', 320.00);
-
-
-INSERT into orders (OrderID,CustomerID,OrderDate,TotalAmount)VALUES
-(6,1,'2025-07-11',100.10);
-
-INSERT into orders (OrderID,CustomerID,OrderDate,TotalAmount)VALUES
-(7,2,'2025-08-11',150.25);
+(5, 5, '2025-09-12', 320.00),
+(6, 1,'2025-07-11', 100.10),
+(7, 2,'2025-08-11', 150.25);
 SELECT * FROM orders;
 
-SELECT o.OrderID , o.OrderDate , o.TotalAmount , c.name , c.email
-FROM orders o 
-JOIN customer c ON o.CustomerID = c.customerID
-WHERE o.CustomerID = 2
+SELECT * FROM orders WHERE CustomerID = 2
 
 UPDATE orders SET TotalAmount=51.25 WHERE CustomerID=4;
-SELECT * FROM orders;
+
 DELETE FROM orders WHERE OrderID = 4;
 SELECT * FROM orders;
 
